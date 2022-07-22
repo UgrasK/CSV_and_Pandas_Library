cb6741c5893874c8bf77439a1e8817ba1899217f
ugrasK <ugurkaraer1989@gmail.com>
2022-07-22 23:03:57 +0300

    how to create dataframe from scratch and how to create a csv from it.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 
 import pandas
 
-data = pandas.read_csv("weather-data.csv")
+# data = pandas.read_csv("weather-data.csv")
 # print(data["temp"])
 
 # this prints Pandas Dataframe
@@ -20,20 +20,34 @@ data = pandas.read_csv("weather-data.csv")
 # print(type(data["temp"]))
 
 # turn dataframe into a dictionary
-data_dict = data.to_dict()
-print(data_dict)
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# # turn series into a list
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# # finding average temperature
+# average = sum(temp_list) / len(temp_list)
+# print(average)
+#
+# # finding average temperature with series methods
+# print(data["temp"].mean())
+#
+# # get data in columns. these are the same
+# print(data["condition"])
+# print(data.condition)
 
-# turn series into a list
-temp_list = data["temp"].to_list()
-print(temp_list)
+# get data in rows. this prints Monday 12 Sunny
+# print(data[data.day == "Monday"])
 
-# finding average temperature
-average = sum(temp_list) / len(temp_list)
-print(average)
+# create a dataframe from scratch
+data_dict = {
+        "students": ["Amy", "James", "Angela"],
+        "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
 
-# finding average temperature with series methods
-print(data["temp"].mean())
-
-# these are the same
-print(data["condition"])
-print(data.condition)
+# create a csv file from a dataframe
+data.to_csv("new_data.csv")
