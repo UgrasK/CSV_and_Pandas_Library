925eed117aa5672a6972290d2f0a311436bac5cb
UgrasK <ugurkaraer1989@gmail.com>
2022-07-23 12:35:07 +0300

    the great squirrel census data analysis with pandas.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
-import pandas
+# import pandas
 
 # data = pandas.read_csv("weather-data.csv")
 # print(data["temp"])
@@ -42,12 +42,34 @@ import pandas
 # print(data[data.day == "Monday"])
 
 # create a dataframe from scratch
-data_dict = {
-        "students": ["Amy", "James", "Angela"],
-        "scores": [76, 56, 65]
-}
-data = pandas.DataFrame(data_dict)
-print(data)
+# data_dict = {
+#         "students": ["Amy", "James", "Angela"],
+#         "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# print(data)
+#
+# # create a csv file from a dataframe
+# data.to_csv("new_data.csv")
 
-# create a csv file from a dataframe
-data.to_csv("new_data.csv")
+import pandas
+
+# copy needed data from csv file and save squirrel's count for their fur color
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+
+print(grey_squirrel_count)
+print(red_squirrel_count)
+print(black_squirrel_count)
+
+# use squirrel's count to create dictionary
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrel_count, red_squirrel_count, black_squirrel_count]
+}
+
+# convert dictionary into dataframe and use that to create a csv
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
